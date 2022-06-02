b0c7e2ea9fff9a8d7707bd192db34cc8041ac762
wjvera <washington666xxx@gmail.com>
2022-06-02 11:35:51 -0500

    animando el menu

--- hamburguesa.js
+++ hamburguesa.js
@@ -1,13 +1,21 @@
 
 
-export default function hamburguerMenu (pnlBtn, panel){
+export default function hamburguerMenu (pnlBtn, panel, quitar){
 
     const d = document
 
     d.addEventListener('click', event =>{
 
+        console.log(event.target)
+
         if (event.target.matches(pnlBtn) || event.target.matches(`${pnlBtn} *`)) {
             d.querySelector(panel).classList.toggle('is-active')
+            d.querySelector(pnlBtn).classList.toggle('is-active')
+        }
+
+        if (event.target.matches(quitar)) {
+            d.querySelector(panel).classList.remove('is-active')
+            d.querySelector(pnlBtn).classList.remove('is-active')
         }
     })
 }
--- principal.js
+++ principal.js
@@ -5,5 +5,5 @@ import hamburguerMenu from "./hamburguesa.js";
 const d = document
 
 d.addEventListener('DOMContentLoaded', (event)=>{
-    hamburguerMenu('.panel-btn', '.panel')
+    hamburguerMenu('.panel-btn', '.panel', '.panel a')
 })
