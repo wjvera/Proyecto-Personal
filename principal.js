c8972c2ccc34d26ebd59df48dc5cb43bbdf13700
wjvera <washington666xxx@gmail.com>
2022-06-04 06:59:10 -0500

    alarma para el reloj

--- principal.js
+++ principal.js
@@ -1,9 +1,18 @@
 
-
+//cuando el export es por default
 import hamburguerMenu from "./hamburguesa.js";
+
+// cuando no es por default 
+import { digitalClock, alarm } from "./reloj.js";
+
 
 const d = document
 
 d.addEventListener('DOMContentLoaded', (event)=>{
+    
     hamburguerMenu('.panel-btn', '.panel', '.panel a')
+
+    digitalClock('#reloj', '#activar-reloj', '#desactivar-reloj')
+
+    alarm('assets/estornudo.mp3', '#activar-alarma', '#desactivar-alarma')
 })
---
+++ reloj.js
@@ -0,0 +1,65 @@
+
+const d = document
+
+
+export function digitalClock(re, btnStart, btnStop) {
+    
+    let guardarHora 
+
+    d.addEventListener('click', event =>{
+
+        //iniciar reloj
+        if (event.target.matches(btnStart)) {
+
+            guardarHora = setInterval(() => {
+                let hora = new Date().toLocaleTimeString()
+                d.querySelector(re).innerHTML = `<h3>${hora}</h3>`
+            }, 1000);
+
+            //deshabilitamos el boton
+            event.target.disabled = true
+        }
+
+
+        // detener reloj
+
+        if (event.target.matches(btnStop)) {
+            clearInterval(guardarHora) // detener el reloj
+            d.querySelector(re).innerHTML = null //desaparecer el selector
+            d.querySelector(btnStart).disabled = false //volvemos a activar el boto
+
+        }
+    })
+}
+
+
+export function alarm(sonido, alarmStart, alarmStop) {
+
+    let alarm
+    const $audio = d.createElement('audio') //creamos un elemento de tipo audio
+
+    //el sonido que le pasamos por parametros se guardara en la propiedad source de audio
+    $audio.src = sonido
+
+    d.addEventListener('click', event =>{
+
+        // iniciar alarma
+        if (event.target.matches(alarmStart)) {
+            alarm = setTimeout(() => {
+                $audio.play()
+            }, 2000);
+
+            //deshabilitamos el boton
+            event.target.disabled = true
+        }
+
+
+
+        // detener alarma
+        if (event.target.matches(alarmStop)) {
+            clearTimeout(alarm) //limpiamos el Timeout
+            alarm.currentTime = 0 //reiniciamos el sonido
+            d.querySelector(alarmStart).disabled = false //volvemos a activar el boton
+        }
+    })
+}
