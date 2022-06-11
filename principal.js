c842789919a05c015abd2b50d824f29617a1bb5c
wjvera <washington666xxx@gmail.com>
2022-06-11 00:22:02 -0500

    user agent

--- principal.js
+++ principal.js
@@ -13,6 +13,7 @@ import subir from "./Modulos/ScrollArriba.js";
 import temaObscuro from "./Modulos/obscuro.js";
 import responsiveMedia from "./Modulos/objetosResponsive.js";
 import tester from "./Modulos/responsiveTester.js";
+import detectar from "./Modulos/deteccionDispositivos.js";
 
 
 const d = document
@@ -33,6 +34,7 @@ d.addEventListener('DOMContentLoaded', (event)=>{
     responsiveMedia('mapa', '(min-width: 700px)', `<a href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63815.42488177145!2d-78.02456551643712!3d-1.4928810999999866!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91d3e0a0cec83d13%3A0x106de38489136d76!2sPuyo!5e0!3m2!1ses-419!2sec!4v1654891739577!5m2!1ses-419!2sec" target='_blank' rel="noopener">Ver mapa</a>`, `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63815.42488177145!2d-78.02456551643712!3d-1.4928810999999866!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91d3e0a0cec83d13%3A0x106de38489136d76!2sPuyo!5e0!3m2!1ses-419!2sec!4v1654891739577!5m2!1ses-419!2sec" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`)
 
     tester('responsive-tester')
+    detectar('info-device')
 })
 
 temaObscuro('.dark-theme-btn', 'dark-mode')
